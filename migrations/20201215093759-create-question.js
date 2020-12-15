4b3b92702a24eba95f757c901cde7c71a047413f
eclerchik13 <ler.poter2014@yandex.ru>
2020-12-15 15:23:48 +0300

    New models Test and Question

---
+++ migrations/20201215093759-create-question.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      questions: {
+        type: Sequelize.JSON
+      },
+      answers: {
+        type: Sequelize.JSON
+      },
+      testId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Questions');
+  }
+};
